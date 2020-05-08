e5134e8407b7ffcd959961338909ace91138d6a9
Santosh kumar <santosh.frev@gmail.com>
2020-05-08 20:22:03 +0530

    Linked List Day 4

---
+++ LinkedList/RotateLL.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node *rotate(struct Node *head, int k);
+
+Node *insert() {
+    int n, i, value;
+    struct Node *temp, *head = NULL;
+    cin >> n;
+
+    for (i = 0; i < n; i++) {
+        cin >> value;
+        if (i == 0) {
+            head = new Node(value);
+            temp = head;
+            continue;
+        } else {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+    return head;
+}
+
+void printList(Node *node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void) {
+
+    int t, k;
+
+    scanf("%d", &t);
+    while (t--) {
+        Node *head = insert(); // insert function to build linked list
+        scanf("%d", &k);
+        head = rotate(head, k); // rotate linked list anti-clockwise  by k nodes
+        printList(head);        // print linked list after rotation
+    }
+    return (0);
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+/*  This function should rotate list counter-clockwise
+    by k and return new head (if changed) */
+Node* rotate(Node* head, int k) {
+    // Your code here
+    Node* curr=head,*kth;
+    k = k-1;
+    while(k--)
+    {
+        curr = curr->next;
+    }
+    kth = curr;
+    while(curr->next!=NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = head;
+    head = kth->next;
+    kth->next = NULL;
+    return head;
+}
