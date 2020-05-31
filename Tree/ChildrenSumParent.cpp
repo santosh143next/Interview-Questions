f222589494cd942661723627e00754888f117ae8
Santosh kumar <santosh.frev@gmail.com>
2020-05-31 21:04:19 +0530

    Tree Day 1 & Day 2

---
+++ Tree/ChildrenSumParent.cpp
@@ -0,0 +1,164 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int isSumProperty(struct Node *node);
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str;
+        getline(cin, str);
+        Node *root = buildTree(str);
+        cout << isSumProperty(root) << "\n";
+
+        
+    }
+
+
+    return 0;
+}// } Driver Code Ends
+
+
+/*Complete the function below
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+// Return 1 if all the nodes in the tree satisfy the given property. Else return 0
+int isSumProperty(Node *root)
+{
+ // Add your code here
+ int left_data=0,right_data=0;
+ if(root==NULL || (root->left==NULL && root->right==NULL))
+ {
+     return 1;
+ }
+ else 
+ {
+     if(root->left!=NULL)
+     {
+         left_data = root->left->data;
+     }
+     if(root->right!=NULL)
+     {
+         right_data = root->right->data;
+     }
+     if((root->data==left_data+right_data) && isSumProperty(root->left) && isSumProperty(root->right))
+     {
+         return 1;
+     }
+     else 
+     return 0;
+ }
+ 
+ 
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
